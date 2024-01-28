7939860103ee71eb619351dcce20ed84756a2157
Zubeda Wajid <zubaidawajid2@gmail.com>
2024-01-27 21:07:08 -0500

    dddfffdd

--- client/src/pages/Network.jsx
+++ client/src/pages/Network.jsx
@@ -21,10 +21,7 @@ export const Network = () => {
 
     <main>
       <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-<<<<<<< HEAD
-    
-=======
->>>>>>> 0407650d828a63ce693efc2cf173c1eb811687d1
+
         <ProgressBar/>
       </div>
 
