6648a7fe92a4afdc60d3841b4d10ad9296a2c799
Constance <knztnce@gmail.com>
2024-01-27 22:57:14 -0500

    the database and its functions

--- server/main.py
+++ server/main.py
@@ -1,9 +1,26 @@
 import nmap
+import logging
+import database
+
+exploits = database.ExploitDB()
+
+data = [
+    ("vsftpd 2.3.4", "exploit/unix/ftp/vsftpd_234_backdoor", ""),
+    ("PostgreSQL DB 8.3.0 - 8.3.7", "exploit/linux/postgres/postgres_payload", "LHOST"),
+]
+
+exploits.add_entry(data)
+
+exploits.retrieve_data("vsftpd 2.3.4")
+
+# print(exploits.retrieve_data("vsftpd 2.3.4"))
+# print(exploits.retrieve_data("merde"))
+
 
 class Network:
     # has a propery of machine objects in a list
     # has a property of the ip range (start and end)
-    
+
     def __init__(self, start_ip, end_ip):
         self.start_ip = start_ip
         self.end_ip = end_ip
@@ -22,13 +39,12 @@ class Network:
 
     def getMachines(self):
         return self.machines
-    
+
     def getStartIp(self):
         return self.start_ip
-    
+
     def getEndIp(self):
         return self.end_ip
-
 
 
 # Create a class to represent a machine
@@ -42,12 +58,12 @@ class Machine:
         # self.vulnerabilities = []
 
     # def runVulnScan(self):
-        # run the vulnerability scan
+    # run the vulnerability scan
 
-    def exploitPort(self):
+    def exploitPort(self, port):
         # run the exploit on the port
         pass
-        
+
     def scan(self, flags):
         # now run a nmap scan on all ports on the machine
         nm = nmap.PortScanner()
@@ -55,7 +71,6 @@ class Machine:
 
         # need to parse the data from the nmap scan
         return nm.all_hosts()
-
 
     def setName(self, name):
         self.name = name
@@ -71,4 +86,3 @@ class Machine:
 
     def getStatus(self):
         return self.status
-
